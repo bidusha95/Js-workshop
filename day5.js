1a6b5762779e762214c981f94a1bfb4a9edecbff
bidusha95 <bidushadahal33@gmail.com>
2024-07-07 15:30:55 +0545

    Day6

---
+++ day5.js
@@ -0,0 +1,75 @@
+//reduce ko parameter ma 2 ta data aauxa
+//reduce le 2 ta argument linxa 
+
+
+// const numm = [1,2,3,4,5]
+// const sum = numm.reduce((accumulator,num)=>{
+//     return num + accumulator 
+
+// },2)
+// console.log(sum)
+
+
+// const products = [
+//     {
+//         product : 'A',
+//         price : 200,
+//         qty : 2
+//     },
+//     {
+//         product : 'B',
+//         price : 300,
+//         qty : 7
+//     },
+//     {
+//         product : 'A',
+//         price : 1100,
+//         qty : 8
+//     }
+// ]
+
+// const totalprice = products.reduce((acc,hello)=>{
+//     return hello.price * hello.qty + acc
+// // },0)
+// // console.log (totalprice)
+// // acc.total = hello.price * hello.qty + acc.total
+// // acc.totalqty = hello.qty  + acc.totalqty
+// // },{total : 0 , totalqty : 0})
+// // console.log(totalprice)
+
+
+// //make a function that accept above reviews & returns the 
+// //average rating
+
+
+// const reviews = [
+//     {
+//         rating : 2
+//     },
+//     {
+//         rating : 4
+//     },
+//     {
+//         rating : 5
+//     }
+// ]
+// function calculateRating (reviews){
+//     const totalrating = reviews.reduce((acc,review)=>{
+//         return review.rating + acc
+//     },0)
+//     const avgrating = totalrating/reviews.length
+//     console.log(avgrating)
+// }
+// calculateRating(reviews)
+
+console.log("hello")
+const h1 = document.getElementsByTagName('h1')[0]
+h1.textContent = 'Hello world'
+
+// case 
+// camelCase --> bidushaDahal
+// first word xai sano hunxa ani tespachi ko aru aaune word ko xai first letter capital hunxa
+// snake_case --> bidusha_dahal
+// kabab-case --> bidusha-dahal
+// PascalCase --> BidushaDahal
+
